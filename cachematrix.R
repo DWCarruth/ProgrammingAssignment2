9242159cb4d622dacdd6e15a54c847ca51145130
DWCarruth <daniel.carruth@gmail.com>
2015-04-26 14:04:38 -0500

    defined functions
    Defined makeCacheMatrix and cacheSolve

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
-
+## makeCacheMatrix creates a special "matrix" 
+## that is really a list containing functions to
+## set value of the matrix
+## get value of the matrix
+## set value of the inverse matrix
+## get value of the inverse matrix
 makeCacheMatrix <- function(x = matrix()) {
-
+  invM <- NULL
+  set <- function(y) {
+    x <<- y
+    invM <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inv) invM <<- inv
+  getinv <- function() invM
+  list(set = set, get = get, 
+       setinv = setinv, 
+       getinv = getinv)
 }
 
-
-## Write a short comment describing this function
+## calculates the inverse of the special "matrix" created
+## with makeCacheMatrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinv()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setinv(inv)
+  inv
 }
